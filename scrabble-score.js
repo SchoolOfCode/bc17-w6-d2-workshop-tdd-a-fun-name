f2c7c68599c8708844a417529995298aa27f32d2
kieranp-p6 <kiip96@icloud.com>
2024-08-06 13:18:36 +0100

    added basic steps

--- scrabble-score.js
+++ scrabble-score.js
@@ -3,3 +3,11 @@ export function calculateScrabbleScore(word) {
         return 1
     }
 }
+
+
+// need to break the "word" to an array
+// loop over array
+// create a switch statement compare letter
+// store a score so if letter === letter give x points
+// accumulate points
+// output the final score
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -2,7 +2,7 @@ import { calculateScrabbleScore } from "./scrabble-score";
 import {test, expect} from "vitest";
 
 
-//calculate the score of a single input
+//calculate the score of a single input ✅
 test('test 1', () =>{
     expect(calculateScrabbleScore("a")).toBe(1)
 })

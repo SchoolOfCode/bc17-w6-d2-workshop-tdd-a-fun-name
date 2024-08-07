7f00d1dde612eb3cef2b213424ac46ca0f5a9994
kieranp-p6 <kiip96@icloud.com>
2024-08-07 09:22:21 +0100

    push

--- scrabble-score.js
+++ scrabble-score.js
@@ -4,6 +4,7 @@ export function calculateScrabbleScore(word) {
 
     let score = 0;
     wordToLetters.forEach(letter => {
+        // switch statement for each letter case
         switch (letter) {
             case 'A':
             case 'E':
@@ -59,63 +60,6 @@ export function calculateScrabbleScore(word) {
 // need to break the "word" to an array ✅
 // loop over array ✅
 // create a switch statement compare letter✅
-/*    switch(word) {
-        case 'A': score = 1
-        break;
-        case 'E': score = 1
-        break;
-        case 'I': score = 1
-        break;
-        case 'L': score = 1
-        break;
-        case 'N': score = 1
-        break;
-        case 'O': score = 1
-        break;
-        case 'R': score = 1
-        break;
-        case 'S': score = 1
-        break;
-        case 'T': score = 1
-        break;
-        case 'U': score = 1
-        break;
-        case 'D': score = 2
-        break;
-        case 'G': score = 2
-        break;
-        case 'C': score = 3
-        break;
-        case 'B': score = 3
-        break;
-        case 'M': score = 3
-        break;
-        case 'P': score = 3
-        break;
-        case 'F': score = 4
-        break;
-        case 'H': score = 4
-        break;
-        case 'V': score = 4
-        break;
-        case 'W': score = 4
-        break;
-        case 'Y': score = 4
-        break;
-        case 'K': score = 5
-        break;
-        case 'J': score = 8
-        break;
-        case 'X': score = 8
-        break;
-        case 'Q': score = 10
-        break;
-        case 'Z': score = 10
-        break;
-        default: console.log("Error, not a valid letter")
-    }
-
- */
-// store a score so if letter === letter give x points
-// accumulate points
-// output the final score
+// store a score so if letter === letter give x points✅
+// accumulate points✅
+// output the final score✅
